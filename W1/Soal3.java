dde223a120750c018ccd2da743fb725be587ac40
Adrian M.R <adrianmadani@gmail.com>
2026-09-11 22:28:05 +0700

    W1

---
+++ W1/Soal3.java
@@ -0,0 +1,11 @@
+package W1;
+
+import java.util.Scanner;
+
+public class Soal3 {
+        public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        System.out.println("Nilai nx: " + nx);
+        }
+}
